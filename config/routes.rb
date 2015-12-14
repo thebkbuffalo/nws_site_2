9026efa04aed78a48d2ed1a66802c5695d3a50e2
Evan Berg <evan.m.berg@gmail.com>
2015-12-14 14:46:46 -0500

    adding login stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
